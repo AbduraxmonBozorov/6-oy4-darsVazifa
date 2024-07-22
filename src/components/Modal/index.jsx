4ef7a24c85b591e0e890440395ce6233674cabbf
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-07-22 17:41:39 +0500

    Modal component pass claass add to id

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -4,17 +4,17 @@ import styles from "./index.module.css";
 function Modal() {
 
     document.addEventListener("click", (event)=>{
-        if(event.target.className=="_modal_1v3qj_1"){
-            document.querySelector("._modal_1v3qj_1").style.display="none";
+        if(event.target.id=="modal"){
+            document.querySelector("#modal").style.display="none";
         }
     })
 
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} id='modal'>
         <h1>Enter img url</h1>
         <input type="url" name="imgUrl" id="imgUrl" placeholder='https://hsdvchjvschjb' />
         <button onClick={()=>{
-            document.querySelector("._modal_1vax0_1").style.display="none"
+            document.querySelector("#modal").style.display="none"
         }} className={styles.close}>X</button>
     </div>
   )
